58e1c507c1b5283424b0c4375be82af143ce3395
Evgeniya <evgeniya-e92@mail.ru>
2022-10-03 13:02:34 +0400

    test commit

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,12 +1,47 @@
 package ru.netology.service;
 
-import org.junit.jupiter.api.Test;
+import org.testng.annotations.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.testng.Assert.*;
 
-class CashbackHackServiceTest {
+public class CashbackHackServiceTest {
+
+
+    CashbackHackService hackService = new CashbackHackService();
 
     @Test
-    void remain() {
+    public void testRemain() {
+
+        assertEquals(hackService.remain(900), 100);
+    }
+
+    @Test
+    public void testRemain2() {
+
+        assertEquals(hackService.remain(999), 1);
+    }
+
+    @Test
+    public void testRemain3() {
+
+        assertEquals(hackService.remain(1_001), 999);
+    }
+
+    @Test
+    public void testRemain4() {
+
+        assertEquals(hackService.remain(1_000), 0);
+    }
+
+    @Test
+    public void testRemain5() {
+
+        assertEquals(hackService.remain(0), 1_000);
+    }
+
+    @Test
+    public void testRemain6() {
+
+        assertEquals(hackService.remain(-100), 0);
     }
 }
